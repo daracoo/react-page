94940683d19dfdda1365f2e386ede998c39607a6
daracoo <dkuzmanoski21@gmail.com>
2024-07-23 15:36:31 +0200

    Rendering Content Conditionally

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import { CORE_CONCEPTS } from './data.js';
 import Header from './components/Header/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
+import { EXAMPLES } from './data.js';
+
 
 function App() {
-  const [selectedTopic, setSelectedTopic] = useState('Please click a button');
+  const [selectedTopic, setSelectedTopic] = useState();
 
   function handleSelect(selectedButton) {
     //selectedButton => 'components', 'jsx', 'props', 'state'
-    setSelectedTopic(selectedButton);  
+    setSelectedTopic(selectedButton);
     console.log(selectedTopic);
   }
   return (
@@ -36,7 +38,19 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          {selectedTopic}
+          {!selectedTopic ? <p>Please select a topic.</p> : null // for page to not crush if we havent selected topic
+          }
+          {selectedTopic ? (
+          <div id='tab-content'>
+                      <h3>{EXAMPLES[selectedTopic].title}</h3>
+                      <p>{EXAMPLES[selectedTopic].description}</p>
+                      <pre>
+                        <code>
+                          {EXAMPLES[selectedTopic].code}
+                        </code>
+                      </pre>
+                    </div>
+          ) : null }
         </section>
       </main>
     </div>
